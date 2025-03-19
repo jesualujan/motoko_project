3cca963994baeb3e2aad0648b39b93a893888d27
jesualujan <lujan.oficial@gmail.com>
2025-03-19 15:51:30 -0600

    🐛 fixed bug

--- src/Crud_frontend/src/agent.js
+++ src/Crud_frontend/src/agent.js
@@ -1,12 +1,12 @@
 import { HttpAgent, Actor } from "@dfinity/agent";
 import { idlFactory } from "../../declarations/Crud_backend"; // Ajusta la ruta según tu proyecto
-import canisterIds from "../../../.dfx/local/canister_ids.json";   // Archivo generado automáticamente
+  // Archivo generado automáticamente
 
 // Configura el agente con la dirección de tu servidor local
 const agent = new HttpAgent({ host: "http://127.0.0.1:4943" });
 
 // Obtén el ID de tu canister local
-const canisterId = canisterIds.motoko_project.local;
+const canisterId = "be2us-64aaa-aaaaa-qaabq-cai";
 
 // Crea un actor para interactuar con el backend
 export const backendActor = Actor.createActor(idlFactory, {
