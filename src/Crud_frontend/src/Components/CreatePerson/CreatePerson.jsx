a56074d084b680ab44635b0f9cf115c91104ade8
jesualujan <lujan.oficial@gmail.com>
2025-03-19 16:18:16 -0600

    ✨ mejoramos el código

---
+++ src/Crud_frontend/src/Components/CreatePerson/CreatePerson.jsx
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import { backendActor } from "../../agent";
+
+
+const CreatePerson = () => {
+    const [name, setName] = useState("");
+    const [profileImage, setProfileImage] = useState("");
+   
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        await backendActor.createPersona(name, profileImage);
+        setName("");
+        setProfileImage("");
+        alert("Persona creada exitosamente!");
+      } catch (error) {
+        console.error("Error al crear la persona:", error);
+        alert("Hubo un error al crear la persona. Revisa la consola.");
+      }
+    };
+
+  return (
+  <>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Nombre"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="URL de la imagen de perfil"
+        value={profileImage}
+        onChange={(e) => setProfileImage(e.target.value)}
+      />
+      <button type="submit">Crear Persona</button>
+    </form>
+    </>
+  
+  );
+};
+
+export default CreatePerson;
